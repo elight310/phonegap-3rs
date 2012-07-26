e0d01de6c137744af512df1b0d3bfc40daf3fbbb
Kevin J <elight.haha@gmail.com>
2012-07-27 00:44:49 +0800

    fix recente view

--- sites/all/modules/three_rs/three_rs.js
+++ sites/all/modules/three_rs/three_rs.js
@@ -8,7 +8,7 @@ function on_click_three_rs_search_submit(term_id, cur_url, base_url) {
 	jQuery.ajax({
 		url: 	_url,
 		type: 	'POST',
-		data:{'term': term, 'mag_q': cur_url, 'mag':'mag' },
+		data:{'term': term, 'mag_q': cur_url/*, 'mag':'mag'*/ },
 		beforeSend: function(jqXHR, settings) {
 
 		},
@@ -21,6 +21,34 @@ function on_click_three_rs_search_submit(term_id, cur_url, base_url) {
 				jQuery('#thr_search_block_result_pane .overview').html(json.result_html);
 				jQuery('#thr_search_block_result_wrapper').dialog('open');
 				jQuery('#thr_search_block_result_pane').tinyscrollbar_update();
+			}
+			else {
+				alert(json.msg);
+			}
+		}	// END OF SUCESS FUNCTION
+	});	
+
+	return false;
+}
+
+function three_rs_get_recent_views(node_id, cur_url, base_url) {
+	var _url = base_url+'/three_rs/get_recent_views';
+	
+	jQuery.ajax({
+		url: 	_url,
+		type: 	'POST',
+		data:{'node_id': node_id, 'mag_q': cur_url/*, 'mag':'mag'*/ },
+		beforeSend: function(jqXHR, settings) {
+
+		},
+		error: function(jqXHR, textStatus, errorThrown) {
+			alert(textStatus);
+		},
+		success: function(response) {
+			eval("var json=" + response + ";");
+			if (json.code == "success") {
+				jQuery('#recent_viewed_topics_pane_wrapper').html(json.result_html);
+				jQuery('#recent_viewed_topics_pane').tinyscrollbar({ sizethumb: 40 });
 			}
 			else {
 				alert(json.msg);
